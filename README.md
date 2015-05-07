6c0bbd860de040af8c7298580c3f60f3c1b4a55b
roby <rob.young@adscale.co.nz>
2015-05-07 13:50:51 +1200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # slack_differ
 a python script that sends messages to a slack webhook when the output from a script changes
+
+## how to get started
+1. run `slack_differ example`
+2. `chmod +x` the example.sh it produces under the new scripts dir
+3. change the configuration in `scripts/example.json` to match your webhook url
+
+## example configuration json
+```json
+{  
+  "script":"example.sh",
+  "title":"script title",
+  "user":"examplebot",
+  "slack_url":"https://hook.slack.com/XXX/YYY",
+  "channel":"#example",
+  "emoji":":metal:"
+}
+```
---
+++ slack_differ.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+import sh
+from os import listdir, mkdir
+from os.path import join, realpath, dirname, isdir, isfile
+import json
+import sys 
+
+dname = dirname(realpath(__file__))
+script_dir = join(dname,'scripts')
+if not isdir(script_dir):
+  print "no script dir found"
+  scripts = []
+else:
+  scripts = [f for f in listdir(script_dir) if f.endswith(".json")]
+for script in scripts:
+  script_metadata = json.load(open(join(script_dir, script)))
+  old_file = join(dname, script + ".old")
+  new_file = join(dname, script + ".new")
+  new_out = open(new_file,'w')
+  command = sh.Command(join(script_dir,script_metadata["script"]))
+  command(_out=new_out)
+  if not isfile(old_file):
+    sh.cp(new_file,old_file)
+  diff = sh.diff(old_file,new_file,_ok_code=[0,1])
+  if len(diff) > 0:
+    message = str(open(new_file).read())
+    payload = {
+        "channel":script_metadata["channel"],
+        "username":script_metadata["user"],
+        "text": script_metadata["title"],
+        "icon_emoji":script_metadata["emoji"],
+        "attachments" : [
+          {
+            "color":"good",
+            "fields":[
+              {
+                "title":"new value",
+                "value":message,
+                "short":False
+                }
+              ]
+            }
+          ]
+        }
+    payload = "payload=" + json.dumps(payload)
+    sh.mv(new_file,old_file)
+    sh.curl(["curl", "-X", "POST" ,"--data-urlencode" ,payload,script_metadata["slack_url"]])
+
+if len(sys.argv) > 1 and sys.argv[1] == "example":
+  print("initialising example")
+  if not isdir(script_dir):
+    mkdir(script_dir)
+  script = open(join(script_dir,"example.sh"),'w')
+  script.write("#!/bin/bash\necho 'hi'")
+  metafile = open(join(script_dir,"example.json"),'w')
+  meta = {
+    "channel":"#example",
+    "user":"examplebot",
+    "title":"script title",
+    "emoji":":metal:",
+    "slack_url":"https://hook.slack.com/XXX/YYY",
+    "script":"example.sh"
+  }
+  metafile.write(json.dumps(meta))
